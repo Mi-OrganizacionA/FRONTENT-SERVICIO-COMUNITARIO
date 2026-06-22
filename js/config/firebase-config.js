628c4b2500e7453a88af1b720719860c7d0b1813
JohanTF21 <johanfertorrealba@gmail.com>
2026-06-22 15:24:01 -0400

    Update firebase-config.js

--- js/config/firebase-config.js
+++ js/config/firebase-config.js
@@ -6,9 +6,6 @@ import { getFunctions } from 'https://www.gstatic.com/firebasejs/12.14.0/firebas
 
 // Reemplaza estas credenciales con las de tu proyecto Firebase.
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
